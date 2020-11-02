5cc97e02521711a9236dc6ad192d66014da492b8
herman <hjb_works@163.com>
2020-11-02 14:34:01 +0800

    Herman first commit

---
+++ OCR.Tool.API/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OCR.Tool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("test")]
+        public JsonResult test()
+        {
+            return new JsonResult("sd");
+        }
+    }
+}
