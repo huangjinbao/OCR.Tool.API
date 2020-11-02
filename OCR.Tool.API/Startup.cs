0fdf9adce5f7dd1a6bd566f91e7094cb00f0a3db
herman <hjb_works@163.com>
2020-11-02 14:38:22 +0800

    herman commit

--- OCR.Tool.API/Startup.cs
+++ OCR.Tool.API/Startup.cs
@@ -12,6 +12,7 @@ namespace OCR.Tool.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
